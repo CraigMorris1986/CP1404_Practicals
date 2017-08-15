0bd33acaf89abdb3c8658455c76a49b192671596
craigmorris1986 <craig.morris1@my.jcu.edu.au>
2017-08-15 20:39:34 +1000

    add valid input function to word_generator.py prac_03

--- prac_03/word_generator.py
+++ prac_03/word_generator.py
@@ -1,11 +1,30 @@
 import random
 VOWELS = "aeiou"
 CONSONANTS = "bcdfghjklmnpqrstvwxyz"
-word_format = "ccvc"
-word = ""
-for kind in word_format:
-    if kind == "c":
-        word += random.choice(CONSONANTS)
-    else:
-        word += random.choice(VOWELS)
-print(word)
+
+
+def is_valid_format(user_letter_choice):
+    for letter in user_letter_choice:
+        if letter != "c" and letter != "v":
+            return False
+    return True
+
+
+def main():
+    is_user_input_valid = False
+    while not is_user_input_valid:
+        print("Invalid input.")
+        word_format = input("Please use c for a random consonant and v for a random vowel\n"
+                            "Example ccvccv : ").lower()
+        is_user_input_valid = is_valid_format(word_format)
+    word = ""
+    for kind in word_format:
+        if kind == "c":
+            word += random.choice(CONSONANTS)
+        else:
+            word += random.choice(VOWELS)
+    print(word)
+
+
+main()
+
