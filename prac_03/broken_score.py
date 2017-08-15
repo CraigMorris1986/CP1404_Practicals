1b9cab8ff4b277f0f8b69cfa0367c4f84c7bf761
craigmorris1986 <craig.morris1@my.jcu.edu.au>
2017-08-15 19:32:28 +1000

    refactor odd_name.py prac_03

---
+++ prac_03/broken_score.py
@@ -0,0 +1,18 @@
+def score_sorter(user_score):
+    """ This function sorts out an integer score value into a print statement. """
+    if user_score < 0 or user_score > 100:
+        return "Invalid score"
+    elif user_score >= 90:
+        return "Excellent"
+    elif user_score >= 50:
+        return "Passable"
+    else:
+        return "Bad"
+
+
+def main():
+    score = float(input("Enter score: "))
+    assessed_grade = score_sorter(score)
+    print(assessed_grade)
+
+main()
