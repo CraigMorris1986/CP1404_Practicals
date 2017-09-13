1b07a849cb05c69b5384b8d1d1a28285427e0644
Craig Morris <craig.morris1@my.jcu.edu.au>
2017-09-13 14:54:36 +1000

    Add box_layout extention work

--- prac_06/box_layout_demo.py
+++ prac_06/box_layout_demo.py
@@ -1,5 +1,6 @@
 from kivy.app import App
 from kivy.lang import Builder
+from kivy.core.window import Window
 
 
 class BoxLayoutDemo(App):
@@ -15,4 +16,19 @@ class BoxLayoutDemo(App):
     def clear_text_field(self):
         self.root.ids.input_name.text = ""
 
-BoxLayoutDemo().run()
+
+class GreeterProgram(App):
+    def build(self):
+        Window.size = (400, 200)
+        self.title = "Greeter Program"
+        self.root = Builder.load_file('greeter_program.kv')
+        return self.root
+
+    def clear_text(self):
+        self.root.ids.text_field.text = ""
+
+    def handle_greet(self):
+        self.root.ids.output_label.text = "Greetings " + self.root.ids.text_field.text
+
+# BoxLayoutDemo().run()
+GreeterProgram().run()
