38088bbf07a9e906af6aaed6ca0693f459487eb1
Craig Morris <craig.morris1@my.jcu.edu.au>
2017-09-19 15:31:31 +1000

    Add car.py and used_cars.py to prac_07

---
+++ prac_07/used_cars.py
@@ -0,0 +1,21 @@
+from prac_07.car import Car
+
+
+def main():
+    my_car = Car("Jeep", 180)
+    my_car.drive(30)
+    print("fuel = {}".format(my_car.fuel))
+    print("Odometer = {}".format(my_car.odometer))
+    print(my_car)
+
+
+def limo():
+    my_limo = Car("Limo", 100)
+    my_limo.add_fuel(20)
+    print(my_limo.fuel)
+    my_limo.drive(115)
+    print(my_limo.odometer)
+    print(my_limo)
+
+main()
+limo()
