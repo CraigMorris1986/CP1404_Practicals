3519b4bc4da7127d87c4df9b0a9bd9e20e0c5dc7
Craig Morris <craig.moris1@my.jcu.edu.au>
2017-10-18 14:24:57 +1000

    Refactor song_lyrics_copyright.py Add find_copyright_free_songs() function to song_lyrics_copyright.py

--- prac_09/song_lyrics_copyright.py
+++ prac_09/song_lyrics_copyright.py
@@ -7,13 +7,8 @@ def main():
 
     for directory in os.listdir("."):
         os.chdir(directory)  # Moves into the required sub-directory.
-        copyright_free_songs[directory] = []
-
-        for file in os.listdir("."):
-            in_file = open(file)
-            if ".i " not in in_file.read():
-               copyright_free_songs[directory].append(file)
-
+        non_copyright_songs = find_copyright_free_songs()
+        copyright_free_songs[directory] = non_copyright_songs
         os.chdir("..")  # Moves up one directory to allow os.chdir() to reassign correct directory.
 
     for song_group, songs in copyright_free_songs.items():
@@ -21,7 +16,15 @@ def main():
         for song in songs:
             print(song)
 
-# def find_copyright_free_songs():
+
+def find_copyright_free_songs():
+    """Searches each file in CWD for the copyright marker and returns a list of files that don't contain it."""
+    searched_songs = []
+    for file in os.listdir("."):
+        in_file = open(file)
+        if ".i " not in in_file.read():
+            searched_songs.append(file)
+    return searched_songs
 
 
 main()
